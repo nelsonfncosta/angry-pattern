4ae05d83600a2d8f594e9c0191298bb284054aa3
Nélson Costa <nelson.fn.costa@gmail.com>
2021-02-05 15:05:17 +0000

    Remove Route exact match

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -33,7 +33,7 @@ export function App(props) {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route path="/" component={HomePage} />
         <Route component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
