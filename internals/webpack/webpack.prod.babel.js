4631ac33c4ba1ca3a5bd2935ebd794b2c1649012
Nélson Costa <nelson.fn.costa@gmail.com>
2021-02-05 15:52:06 +0000

    Update project name

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -122,9 +122,9 @@ module.exports = require('./webpack.base.babel')({
     }),
 
     new WebpackPwaManifest({
-      name: 'React Boilerplate',
-      short_name: 'React BP',
-      description: 'My React Boilerplate-based project!',
+      name: 'Angry Pattern',
+      short_name: 'Angry Pattern',
+      description: 'Angry Pattern',
       background_color: '#fafafa',
       theme_color: '#b1624d',
       inject: true,
